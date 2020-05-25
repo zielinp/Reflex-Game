542812546b1b99368b6873e13df6527a975bd1a3
Paulina Zielinska <pzielinska.zielona@gmail.com>
2020-05-25 20:33:14 +0200

    Improving README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+# JS Reflex Game
+> Class based game in pure JS
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Status](#status)
+* [Contact](#contact)
+
+## General info
+Game project created as part of learning JavaScript based on object oriented programming.
+
+## Technologies
+* JavaScript
+
+## Status
+Project is: _in progress_, not fully functional yet
+
+## Contact
+Created by [@zielinp](https://www.linkedin.com/in/zielinp/) - feel free to contact me!
--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-// const div = document.querySelector(".button-panel");
- ////=====================================//////
-
-class InfoArea{
-  constructor(id){
+////=====================================//////
+class InfoArea {
+  constructor(id) {
     this.id = id;
     this.div = document.createElement("div");
     this.parent = document.querySelector(".top-panel");
   }
 
-  createArea(){
+  createArea() {
     this.div.id = this.id;
     this.parent.appendChild(this.div);
     this.div.classList.add("info-area");
@@ -25,21 +23,21 @@ let time_area = new InfoArea("timeArea");
 time_area.createArea();
 
 
- ////=====================================//////
+////=====================================//////
 class MyBtn {
-  constructor(id, innerText, color){
-    this.button= document.createElement("button");
+  constructor(id, innerText, color) {
+    this.button = document.createElement("button");
     this.id = id;
     this.innerText = innerText;
     this.color = color;
     this.parent = document.querySelector(".button-panel");
   }
 
-  createButton(){
+  createButton() {
     this.button.id = this.id;
     this.button.innerText = this.innerText;
     this.parent.appendChild(this.button);
-    this.button.style.setProperty("background-color",this.color);
+    this.button.style.setProperty("background-color", this.color);
     this.button.classList.add("reactions-buttons");
   }
 }
@@ -50,98 +48,101 @@ start_button.createButton();
 const reset_button = new MyBtn("resetButton", "Reset", "pink");
 reset_button.createButton();
 
- ////=====================================//////
+////=====================================//////
 
-class GameBtn{
-  constructor(parent){
-    this.button= document.createElement("button");
+class GameBtn {
+  constructor(parent) {
+    this.button = document.createElement("button");
     this.parent = parent;
     this.is_active = false;
   }
 
-  createButton(){
+  createButton() {
     this.parent.appendChild(this.button);
     this.button.classList.add("game-buttons");
   }
 
-  activate(color){
-    this.button.style.setProperty("background-color",color);
+  activate(color) {
+    this.button.style.setProperty("background-color", color);
     this.is_active = true;
   }
 
-  deactivate(){
-    this.button.style.setProperty("background-color","white");
+  deactivate() {
+    this.button.style.setProperty("background-color", "white");
     this.is_active = false;
   }
 
 
-  checkState(){
-    this.button.addEventListener("click",() => {
+  checkState() {
+    this.button.addEventListener("click", () => {
       console.log(this.is_active);
-      if(this.is_active == true){
-        licznikPunktow ++;
+      if (this.is_active == true) {
+        licznikPunktow++;
         document.querySelector("#scoresArea").innerText = `Punkty: ${licznikPunktow}`;
+      } else {
+        licznikZyc--;
+        document.querySelector("#lifeArea").innerText = `Życia: ${licznikZyc}`;
+        if (licznikZyc === 0) {
+          alert("Koniec gry, bo koniec zyc");
+          clearInterval(time);
+          clearInterval(proba);
+        }
       }
-      else {
-          licznikZyc--;
-          document.querySelector("#lifeArea").innerText = `Życia: ${licznikZyc}`;
-          if (licznikZyc === 0){
-            alert("Koniec gry, bo koniec zyc");
-          }
-      }
-    }
-  );
+    });
   }
 
 }
 ////=======================////
 
-let licznikZyc=3;
+let licznikZyc = 3;
 
-let licznikPunktow=0;
+let licznikPunktow = 0;
 
-let table_board=new Array();
+let table_board = new Array();
 
-for(let i=0; i<25; i++){
-  table_board[i]=new GameBtn(document.querySelector(".game-board-container"));
+for (let i = 0; i < 25; i++) {
+  table_board[i] = new GameBtn(document.querySelector(".game-board-container"));
   table_board[i].createButton();
   console.log(table_board[i]);
   table_board[i].checkState();
 }
 
 
-function generateRandom(){
-  let i = Math.floor(Math.random()*25);
+function generateRandom() {
+  let i = Math.floor(Math.random() * 25);
   table_board[i].activate("green");
   console.log(table_board[i]);
 
-  window.setTimeout(function(){
+  window.setTimeout(function() {
     table_board[i].deactivate();
   }, 2000);
 }
 
+var proba; // do funkcji generateRandom
 
-function stoper(seconds){
-    document.querySelector("#timeArea").innerText = `Pozostały czas: ${seconds}s`;
-  setInterval(function(){
+var time;
+
+function stoper(seconds) {
+  document.querySelector("#timeArea").innerText = `Pozostały czas: ${seconds}s`;
+  time = setInterval(function() {
     seconds--;
     document.querySelector("#timeArea").innerText = `Pozostały czas: ${seconds}s`;
 
-    if (seconds <=0){
+    if (seconds <= 0) {
       clearInterval(time);
+      clearInterval(proba);
       alert("Koniec gry, bo koniec czasu");
     }
-  },1000);
+  }, 1000);
 }
 
 
 
-document.getElementById("startButton").addEventListener("click", function(){
+document.getElementById("startButton").addEventListener("click", function() {
 
   stoper(60);
   document.querySelector("#scoresArea").innerText = `Punkty: ${licznikPunktow}`;
   document.querySelector("#lifeArea").innerText = `Życia: ${licznikZyc}`;
   generateRandom();
-  window.setInterval(generateRandom, 3000);
-  }
-);
+  proba = setInterval(generateRandom, 3000);
+});
